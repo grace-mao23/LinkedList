8ff0bc5e0aab9e0d6496a560201ed8a9b524b09e
grace-mao23 <gmao00@stuy.edu>
2019-10-23 15:33:34 -0400

    Fixed stuff

--- list.c
+++ list.c
@@ -4,7 +4,7 @@
 
 void print_list(struct node * n) {
   printf("[ ");
-  while (n) {
+  while (n->next && n) {
     printf("%d ", n->i);
     n = n->next;
   }
@@ -13,16 +13,21 @@ void print_list(struct node * n) {
 
 struct node * insert_front(struct node * n, int i) {
   struct node *p = malloc(sizeof(n));
+  //printf("%p\n", p);
   p->i = i;
   p->next = n;
   return p;
 }
 
 struct node * free_list(struct node * n) {
-  while(n) {
-    struct node *p = n;
+  struct node *p = n;
+  while(n->next) {
+    //printf("%p\n", p);
+    p = p->next;
+    n-> next = 0;
     free(n);
-    n = p->next;
+    n = p;
   }
-  return n;
+  n=NULL;
+  return p;
 }
