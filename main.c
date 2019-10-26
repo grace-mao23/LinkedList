a8bda2c1e4b398fe8726938e964fd11415f71fa2
grace-mao23 <gmao00@stuy.edu>
2019-10-26 00:18:10 -0400

    Testing other driver

--- main.c
+++ main.c
@@ -3,6 +3,82 @@
 #include "list.h"
 
 int main() {
+  struct node *n = NULL;
+  //printf("%lu\n", sizeof(struct node)); //returns 16
+  printf("Printing empty list\n");
+  print_list(n);
+  printf("N currently points to %p\n", n);
+
+  /*
+  printf("Printing list with five\n");
+  n = insert_front(n, 5);
+  print_list(n);
+  */
+
+  printf("Adding #s 0-9 to list.\n");
+  int i;
+  for (i = 0; i < 10; i++){
+    n = insert_front(n, i);
+  }
+
+  printf("Printing list:\n");
+  print_list(n);
+
+  printf("Freeing list.\n");
+  n = free_list(n);
+  printf("now N points to: %p\n", n);
+
+  printf("Printing list:\n");
+  print_list(n);
+
+  printf("Adding #s 10-19 to list.\n");
+  for (i = 10; i < 20; i++){
+    n = insert_front(n, i);
+  }
+
+  printf("Printing list:\n");
+  print_list(n);
+
+  printf("Removing nodes:\n");
+  printf("N currently points to %p\n", n);
+  printf("Removing 19: ");
+  n = removeNode(n, 19);
+  print_list(n);
+  printf("now N points to %p\n", n);
+  printf("Removing 19: ");
+  n = removeNode(n, 19);
+  print_list(n);
+  printf("now N points to %p\n", n);
+  printf("Removing 17: ");
+  n = removeNode(n, 17);
+  print_list(n);
+  printf("now N points to %p\n", n);
+  printf("Removing 15: ");
+  n = removeNode(n, 15);
+  print_list(n);
+  printf("now N points to %p\n", n);
+  printf("Removing 10: ");
+  n = removeNode(n, 10);
+  print_list(n);
+  printf("now N points to %p\n", n);
+  printf("Removing 18: ");
+  n = removeNode(n, 18);
+  print_list(n);
+  printf("now N points to %p\n", n);
+
+  printf("Freeing list.\n");
+  n = free_list(n);
+  printf("now N points to: %p\n", n);
+
+  printf("Adding 5 to list.\n");
+  n = insert_front(n, 5);
+  print_list(n);
+  printf("now N points to %p\n", n);
+  printf("Removing 5: ");
+  n = removeNode(n, 5);
+  print_list(n);
+  printf("now N points to %p\n", n);
+
   /*struct node n;
   struct node *p = &n;
   p = NULL;
